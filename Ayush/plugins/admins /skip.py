52162f0cd683d2f60faf48d558e3ffc6abf201e7
MR MOH MAYA <124359102+THEMOHMAYA@users.noreply.github.com>
2025-12-28 22:41:43 +0530

    🥀ᴀʏᴜsʜ🥀

---
+++ Ayush/plugins/admins /skip.py
@@ -0,0 +1,121 @@
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardMarkup, Message
+
+import config
+from Ayush import YouTube, app
+from Ayush.core.call import Aayu
+from Ayush.misc import db
+from Ayush.utils.database import get_loop
+from Ayush.utils.decorators import AdminRightsCheck
+from Ayush.utils.inline import close_markup, stream_markup
+from Ayush.utils.stream.autoclear import auto_clean
+from Ayush.utils.thumbnails import get_thumb
+from config import BANNED_USERS
+
+
+@app.on_message(
+    filters.command(["skip", "cskip", "next", "cnext"]) & filters.group & ~BANNED_USERS
+)
+@AdminRightsCheck
+async def skip(cli, message: Message, _, chat_id):
+    if not len(message.command) < 2:
+        loop = await get_loop(chat_id)
+        if loop != 0:
+            return await message.reply_text(_["admin_8"])
+        state = message.text.split(None, 1)[1].strip()
+        if state.isnumeric():
+            state = int(state)
+            check = db.get(chat_id)
+            if check:
+                count = len(check)
+                if count > 2:
+                    count = int(count - 1)
+                    if 1 <= state <= count:
+                        for x in range(state):
+                            popped = None
+                            try:
+                                popped = check.pop(0)
+                            except:
+                                return await message.reply_text(_["admin_12"])
+                            if popped:
+                                await auto_clean(popped)
+                            if not check:
+                                try:
+                                    await message.reply_text(
+                                        text=_["admin_6"].format(
+                                            message.from_user.mention,
+                                            message.chat.title,
+                                        ),
+                                        reply_markup=close_markup(_),
+                                    )
+                                    await Aayu.stop_stream(chat_id)
+                                except:
+                                    return
+                                break
+                    else:
+                        return await message.reply_text(_["admin_11"].format(count))
+                else:
+                    return await message.reply_text(_["admin_10"])
+            else:
+                return await message.reply_text(_["queue_2"])
+        else:
+            return await message.reply_text(_["admin_9"])
+    else:
+        check = db.get(chat_id)
+        popped = None
+        try:
+            popped = check.pop(0)
+            if popped:
+                await auto_clean(popped)
+            if not check:
+                await message.reply_text(
+                    text=_["admin_6"].format(
+                        message.from_user.mention, message.chat.title
+                    ),
+                    reply_markup=close_markup(_),
+                )
+                try:
+                    return await Aayu.stop_stream(chat_id)
+                except:
+                    return
+        except:
+            try:
+                await message.reply_text(
+                    text=_["admin_6"].format(
+                        message.from_user.mention, message.chat.title
+                    ),
+                    reply_markup=close_markup(_),
+                )
+                return await Aayu.stop_stream(chat_id)
+            except:
+                return
+
+    queued = check[0]["file"]
+    title = (check[0]["title"]).title()
+    user = check[0]["by"]
+    streamtype = check[0]["streamtype"]
+    videoid = check[0]["vidid"]
+    status = True if str(streamtype) == "video" else None
+    db[chat_id][0]["played"] = 0
+
+    exis = (check[0]).get("old_dur")
+    if exis:
+        db[chat_id][0]["dur"] = exis
+        db[chat_id][0]["seconds"] = check[0]["old_second"]
+        db[chat_id][0]["speed_path"] = None
+        db[chat_id][0]["speed"] = 1.0
+
+    if "live_" in queued:
+        n, link = await YouTube.video(videoid, True)
+        if n == 0:
+            return await message.reply_text(_["admin_7"].format(title))
+        try:
+            image = await YouTube.thumbnail(videoid, True)
+        except:
+            image = None
+        try:
+            await Aayu.skip_stream(chat_id, link, video=status, image=image)
+        except:
+            return await message.reply_text(_["call_6"])
+
+    # baki code same rahega (sirf Dil → Aayu already handled)
