9220f8e87cbff1dbcd91190367979f163ccd38ad
MR-SYSTEM-0 <mrhari659@gmail.com>
2025-12-29 14:36:17 +0530

    Update play.py

--- Ayush/utils/inline/play.py
+++ Ayush/utils/inline/play.py
@@ -1,7 +1,7 @@
 import math
 from config import SUPPORT_CHAT, OWNER_ID
 from pyrogram.types import InlineKeyboardButton
-from AarohiX.utils.formatters import time_to_seconds
+from Ayush.utils.formatters import time_to_seconds
 
 
 # -------------------------------- TRACK MARKUP -------------------------------- #
