4e410753508c8029a1716eaf7f68a5d802046043
MR MOH MAYA <124359102+THEMOHMAYA@users.noreply.github.com>
2025-12-29 13:26:16 +0530

    Update bot.py

--- Ayush/core/bot.py
+++ Ayush/core/bot.py
@@ -10,7 +10,7 @@ class Aayu(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            name="Ayushxaaru",
+            name="Ayush",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
