f20b1ed45638f3db88aba03597ab0a566641b42d
MR MOH MAYA <124359102+THEMOHMAYA@users.noreply.github.com>
2026-01-02 16:59:00 +0530

    🥀ᴀʏᴜsʜ🥀

--- Ayush/utils/inline/play.py
+++ Ayush/utils/inline/play.py
@@ -33,7 +33,7 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "◉—————————"
+        bar = "▷—————————"
     elif 10 < umm < 20:
         bar = "—◉————————"
     elif 20 <= umm < 30:
@@ -51,7 +51,7 @@ def stream_markup_timer(_, chat_id, played, dur):
     elif 80 <= umm < 95:
         bar = "————————◉—"
     else:
-        bar = "—————————◉"
+        bar = "—————————▶"
     buttons = [
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
